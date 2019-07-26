a52793c3ef17165e69bce1842972a976baa89fcf
Glenugie <s.cauvin.10@aberdeen.ac.uk>
2019-07-26 12:57:40 +0100

    Removal of old files

--- PCD-PeerSim/res/_Graphs.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:/Users/Sam/Tools/R/R-3.3.1/bin/R.exe" CMD BATCH  --no-save --no-restore "C:/Users/Sam/Dropbox/PhD/PCD-PeerSim/res/graphs.r" "C:/Users/Sam/Dropbox/PhD/PCD-PeerSim/res/graphs-log.txt"
